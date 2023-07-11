eb24207a77be7a71ec1c7fdc39071292ac50c81b
Henn <HennOgyrchik@gmail.com>
2023-07-11 21:02:16 +0300

    hw 6 update

--- hw6/src/Main.java
+++ hw6/src/Main.java
@@ -17,12 +17,29 @@ public class Main {
         vasya.awards.add("Самый лучший кот");
         vasya.awards.add("Мисс вселенная 2023");
 
+
+        vasya.showInfo();
         System.out.printf("Кот %s допущен к участию: %b\n", vasya.name,vasya.checkAdmission());
         vasya.photo();
         vasya.addAward("Милашка года 2023");
         vasya.feed(0.5);
         vasya.disqualification();
         System.out.printf("Кот %s допущен к участию: %b\n", vasya.name,vasya.checkAdmission());
+
+        Cat murzik = new Cat();
+        murzik.name="Мурзик";
+        murzik.breed="Сиамская";
+        murzik.color="Стандартный";
+        murzik.owner="Люся";
+        murzik.gender="male";
+        murzik.age=5;
+        murzik.weight=7.0;
+        murzik.awards.add("Пышечка-пухляшечка <3");
+
+        murzik.showInfo();
+
+
+
     }
 }
 
@@ -61,5 +78,17 @@ class Cat {
     void feed (double mass){
         this.weight+=mass;
         System.out.printf("Кот %s успешно покормлен! Текущий вес кота %.2f кг\n",this.name, this.weight);
+    }
+
+    public void showInfo() {
+        System.out.printf("1) Кличка: %s\n" +
+                "2) Порода: %s\n" +
+                "3) Окрас: %s\n" +
+                "4) Возраст: %d\n" +
+                "5) Пол: %s\n" +
+                "6) Вес: %.2f\n" +
+                "7) Владелец: %s\n" +
+                "8) Награды: %s\n" +
+                "9) Дисквалифицирован: %b\n", this.name,this.breed,this.color, this.age,this.gender,this.weight,this.owner,this.awards,this.disqualification);
     }
 }
